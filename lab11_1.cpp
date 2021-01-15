150dc7a832da5f519afbe076bd40b3881a304664
earthx15 <uncles1512@hotmail.com>
2021-01-15 15:20:19 +0700

    pass

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+
+    cout << fibonacci(50);
+
     return 0; 
+
 }
 
-int fibonacci(int x){
+long long int fibonacci(long long int x){
     if(x>1){
         return fibonacci(x-1)+fibonacci(x-2);
     }else if(x==1){
